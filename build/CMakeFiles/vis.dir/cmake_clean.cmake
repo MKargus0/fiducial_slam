0e220a61a1d159b7e0887e6da00d2f7ea3c098a9
MKargus0 <colodochca@gmail.com>
2021-01-29 18:31:54 +0300

    ok

---
+++ build/CMakeFiles/vis.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vis.dir/src/vision_systems.cpp.o"
+  "libvis.pdb"
+  "libvis.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vis.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
